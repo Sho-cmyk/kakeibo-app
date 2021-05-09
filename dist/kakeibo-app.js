278a07811d438088d193db4290a498895b0361ae
Shunnosuke Fujii <u270077f@alumni.osaka-u.ac.jp>
2021-05-09 16:44:26 +0900

    check

--- dist/kakeibo-app.js
+++ dist/kakeibo-app.js
@@ -377,3 +377,26 @@ function setMibunrui() {
         subcategory.appendChild(op);
     });
 }
+var data = [
+    {
+     value: 300,
+     color:"#F7464A",
+     highlight: "#FF5A5E",
+     label: "Red"
+    },
+    {
+     value: 50,
+     color: "#46BFBD",
+     highlight: "#5AD3D1",
+     label: "Green"
+    },
+    {
+     value: 100,
+     color: "#FDB45C",
+     highlight: "#FFC870",
+     label: "Yellow"
+    }
+   ];
+   
+   var myChart = new Chart(document.getElementById("pie").getContext("2d")).Doughnut(data);
+   
